c60d249788c359ae28e8aaa7698ff2babb64c98c
secure-psis <116293257+secure-psis@users.noreply.github.com>
2023-03-14 21:25:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
   const offerText = document.getElementById("offer-text");
-  let text = "We are pleased to offer you the position of <b><i>Senior Graphic Designer</i></b> at <b>Pie Star Interactive Studios</b> starting on <b><i>02/15/2023</i></b>.\n\n"+
+  let text = "Dear <b> Ayush Kumar Jha</b>,\n\n"+
+            "We are pleased to offer you the position of <b><i>Senior Graphic Designer</i></b> at <b>Pie Star Interactive Studios</b> starting on <b><i>02/15/2023</i></b>.\n\n"+
 
             "<b>Responsibilities:</b>\n\n"+
-            "- Design and create visually appealing graphics for various mediums including print and digital.\n\n"+
-            "- Collaborate with cross-functional teams to gather requirements and provide design solutions.\n\n"+
-            "- Stay up-to-date with industry trends and apply them in your work.\n\n"+
-            "- Present designs and concepts to stakeholders for feedback and approval.\n\n"+
-            "- Revise designs based on feedback to meet project requirements.\n\n"+
+            "As an SDE Developer at Pie Star Interactive Studios, you will be responsible for developing \n\n"+
+            "and maintaining software applications and systems. You will work closely with other \n\n"+
+            "developers, designers, and project managers to create high-quality products that meet the needs of our clients.\n\n"+
+           
+            "\n\n"+
+      
+            "We offer a dynamic and challenging work environment, opportunities for growth and career\n\n"+
+            "advancement, and a competitive compensation package that includes benefits such as\n\n"+
+            "health insurance, paid time off, and retirement savings plans.\n\n"+
             
-            "<b>Qualifications:</b>\n\n"+
-            "- A minimum of 2 years of professional experience as a graphic designer.\n\n"+
-            "- Proficiency in design software such as Adobe Creative Suite.\n\n"+
-            "- Strong attention to detail and ability to meet deadlines.\n\n"+
-            "- Excellent communication and interpersonal skills.\n\n"+
+            "\n\n"+           
+       
+            "We are confident that you will find our company to be a rewarding and fulfilling place to \n\n"+
+            "work, and we look forward to having you as a member of our team. If you accept this offer,\n\n"+
+            "please sign and return the enclosed copy of this letter to indicate your acceptance of the\n\n"+
+            "terms and conditions of your employment.\n\n"+
+           
             
-            "<b>Terms and Conditions:</b>\n\n"+
-            "- You will be required to sign a confidentiality agreement.\n\n"+
-            "- You will be expected to adhere to the company's code of conduct and ethics policy.\n\n"+
-            "- This offer is contingent upon completion of a background check.\n\n"+
-            "- This offer letter constitutes the entire agreement between you and the company.\n\n"+
-            "- The company reserves the right to modify or terminate this offer at any time.\n\n"+
-            
-            "We are confident that you will be a valuable addition to our team and we look forward to welcoming you on board.\n\n"+
+            "Please let us know if you have any questions or concerns. We are excited to have you on board and look forward to working with you.\n\n"+
             
             "Sincerely,\n\n"+
             "<b>Vikas Sharma</b>\n\n"+
-            "<b><i>Co-Founder & CIO</i></b>\n\n"+
+            "<b><i>Co-Founder & CEO</i></b>\n\n"+
             "<b>Pie Star Interactive Studios</b>\n\n";
   
   let index = 0;

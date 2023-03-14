431e5c0a392f3286fa3db5cb86d0d65fea4c15a3
secure-psis <116293257+secure-psis@users.noreply.github.com>
2023-03-14 22:00:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,23 +5,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
            "\n\n"+
       
-            "As an SDE I at Pie Star Interactive Studios, you will be responsible for developing \n\n"+
-            "and maintaining software applications and systems. You will work closely with other \n\n"+
-            "developers, designers, and project managers to create high-quality products that meet the needs of our clients.\n\n"+
-           
+            "As an SDE I at Pie Star Interactive Studios, you will be responsible for developing and maintaining software applications and systems. You will work closely with other developers, designers, and project managers to create high-quality products that meet the needs of our clients.\n\n"+
+            
             "\n\n"+
       
-            "We offer a dynamic and challenging work environment, opportunities for growth and career\n\n"+
-            "advancement, and a competitive compensation package that includes benefits such as\n\n"+
-            "health insurance, paid time off, and retirement savings plans.\n\n"+
-            
+            "We offer a dynamic and challenging work environment, opportunities for growth and career advancement, and a competitive compensation package that includes benefits such as health insurance, paid time off, and retirement savings plans.\n\n"+
+                       
             "\n\n"+           
        
-            "We are confident that you will find our company to be a rewarding and fulfilling place to \n\n"+
-            "work, and we look forward to having you as a member of our team. If you accept this offer,\n\n"+
-            "please sign and return the enclosed copy of this letter to indicate your acceptance of the\n\n"+
-            "terms and conditions of your employment.\n\n"+
-           
+            "We are confident that you will find our company to be a rewarding and fulfilling place to work, and we look forward to having you as a member of our team. If you accept this offer, please sign and return the enclosed copy of this letter to indicate your acceptance of the terms and conditions of your employment.\n\n"+
+                    
             
             "Please let us know if you have any questions or concerns. We are excited to have you on board and look forward to working with you.\n\n"+
             
